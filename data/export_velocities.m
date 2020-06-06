76def2b4a722be8fe4b7ba2410c2721ef247e209
Dominik Stiller <domi.stiller@gmail.com>
2020-06-06 22:11:58 +0200

    Add abstract, introduction and conclusion

--- data/export_velocities.m
+++ data/export_velocities.m
@@ -1,9 +1,15 @@
+function export_velocities()
+
 [path, ~, ~] = fileparts(mfilename('fullpath'));
 
+file = 'PoST_2019-10-03_RUN002';
 time_vector = (100:0.001:500);
-ts_base = timeseries(zeros(length(time_vector), 1), time_vector);
 
-ekf_data = generate_ekf_data('PoST_2019-10-03_RUN002', ts_base, dt);
+% Disable motor speed lowpass filter first
+ts_base = timeseries(zeros(length(time_vector), 1), time_vector);
+ekf_data = generate_ekf_data(file, ts_base, 0.001);
 
 export = [time_vector; ekf_data.z(3,:); ekf_data.z(5,:); ekf_data.z(6,:)]';
-dlmwrite([path, '/', 'velocities.csv'], export, 'delimiter', ',', 'precision', 9);
+dlmwrite([path, '/', 'velocities_', file ,'.csv'], export, 'delimiter', ',', 'precision', 9);
+
+end
