b5f74da404d0004ec2353f70bf79d55e65fa1082
Dominik Stiller <domi.stiller@gmail.com>
2020-06-08 13:38:02 +0200

    Update symbols list and add EKF appendix

--- data/export_nssr.m
+++ data/export_nssr.m
@@ -8,7 +8,12 @@ time_vector = evalin('base', 'tout');
 data = get(logsout, 'wssr');
 nssr = squeeze(data.Values.Data);
 
-export = [time_vector, nssr'];
+data1 = get(logsout, 'EKF_x_1');
+data2 = get(logsout, 'EKF_x_2');
+data3 = get(logsout, 'EKF_x_3');
+x_vx = [data1.Values.Data(:,3), data2.Values.Data(:,3), data3.Values.Data(:,3)];
+
+export = [time_vector, nssr', x_vx];
 dlmwrite([path, '/', 'failure_detection_nssr.csv'], export, 'delimiter', ',', 'precision', 9);
 
 end
